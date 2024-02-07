2163e0c73958efa43b63ea7ffd6f8ca087a80d73
Dominik Witschard <dominik.witschard@zuehlke.com>
2024-02-07 09:59:33 +0100

    formatting: added prettier

--- src/app/components/heroes/heroes.component.ts
+++ src/app/components/heroes/heroes.component.ts
@@ -1,16 +1,24 @@
-import { Component } from '@angular/core';
-import {Hero} from "./heroes.types";
+import { Component } from '@angular/core'
+import { Hero } from './heroes.types'
 
 @Component({
-  selector: 'app-heroes',
-  templateUrl: './heroes.component.html',
-  styleUrl: './heroes.component.scss'
+    selector: 'app-heroes',
+    template: `
+        <h2>{{ hero.name | uppercase }} Details</h2>
+
+        <div><span>id: </span>{{ hero.id }}</div>
+        <div><span>name: </span>{{ hero.name }}</div>
+
+        <div>
+            <label for="name">Hero name: </label>
+            <input id="name" [(ngModel)]="hero.name" placeholder="name" />
+        </div>
+    `,
+    styleUrl: './heroes.component.scss'
 })
 export class HeroesComponent {
-
-  public hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  }
-
+    public hero: Hero = {
+        id: 1,
+        name: 'Windstorm'
+    }
 }
