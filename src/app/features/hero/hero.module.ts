30a5d7a36d529d9afe0fe4d819b696f613e2af00
Dominik Witschard <dominik.witschard@zuehlke.com>
2024-02-07 11:25:20 +0100

    toh-pt4: refactored usage of `CommonModule`

--- src/app/features/hero/hero.module.ts
+++ src/app/features/hero/hero.module.ts
@@ -3,13 +3,12 @@ import { SharedModule } from '@shared/shared.module'
 import { HeroesComponent } from './components/heroes/heroes.component'
 import { HeroDetailsComponent } from './components/hero-details/hero-details.component'
 import { FormsModule } from '@angular/forms'
-import { CommonModule } from '@angular/common'
 
 const EXPORTED_COMPONENTS: Array<Type<any> | any[]> = [HeroesComponent]
 
 @NgModule({
   declarations: [...EXPORTED_COMPONENTS, HeroDetailsComponent],
-  imports: [CommonModule, SharedModule, FormsModule],
+  imports: [SharedModule, FormsModule],
   exports: [...EXPORTED_COMPONENTS]
 })
 export class HeroModule {}
