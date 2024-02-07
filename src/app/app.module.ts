ed7992a4dcd81c7b13d75ff5fc47f87e569e7930
Dominik Witschard <dominik.witschard@zuehlke.com>
2024-02-07 10:00:39 +0100

    formatting: reformatted existing code base with prettier

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,15 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core'
+import { BrowserModule } from '@angular/platform-browser'
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { HeroesComponent } from './components/heroes/heroes.component';
-import { FormsModule } from '@angular/forms';
+import { AppRoutingModule } from './app-routing.module'
+import { AppComponent } from './app.component'
+import { HeroesComponent } from './components/heroes/heroes.component'
+import { FormsModule } from '@angular/forms'
 
 @NgModule({
   declarations: [AppComponent, HeroesComponent],
   imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
-  bootstrap: [AppComponent],
+  bootstrap: [AppComponent]
 })
 export class AppModule {}
--- src/app/components/heroes/heroes.component.ts
+++ src/app/components/heroes/heroes.component.ts
@@ -2,23 +2,23 @@ import { Component } from '@angular/core'
 import { Hero } from './heroes.types'
 
 @Component({
-    selector: 'app-heroes',
-    template: `
-        <h2>{{ hero.name | uppercase }} Details</h2>
+  selector: 'app-heroes',
+  template: `
+    <h2>{{ hero.name | uppercase }} Details</h2>
 
-        <div><span>id: </span>{{ hero.id }}</div>
-        <div><span>name: </span>{{ hero.name }}</div>
+    <div><span>id: </span>{{ hero.id }}</div>
+    <div><span>name: </span>{{ hero.name }}</div>
 
-        <div>
-            <label for="name">Hero name: </label>
-            <input id="name" [(ngModel)]="hero.name" placeholder="name" />
-        </div>
-    `,
-    styleUrl: './heroes.component.scss'
+    <div>
+      <label for="name">Hero name: </label>
+      <input id="name" [(ngModel)]="hero.name" placeholder="name" />
+    </div>
+  `,
+  styleUrl: './heroes.component.scss'
 })
 export class HeroesComponent {
-    public hero: Hero = {
-        id: 1,
-        name: 'Windstorm'
-    }
+  public hero: Hero = {
+    id: 1,
+    name: 'Windstorm'
+  }
 }
